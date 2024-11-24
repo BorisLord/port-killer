c8ba3d3f143b79fb5f54ddf40a617670277b5bea
BorisPad <borisdelord@proton.me>
2024-11-24 22:07:05 +0100

    add readme and finish script killer

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # port-killer
-Simple port killer script
+Simple port killer Bash script
+
+Need ```lsof``` command
+
+This requires a port number as an argument from 0 to 65535  
+```./port_killer.sh 8080```
+
+Make me executable  
+```chmod +x port_killer.sh```
+
--- port_killer.sh
+++ port_killer.sh
@@ -11,3 +11,17 @@ if ! [[ "$port" =~ ^[0-9]+$ ]] || [ "$port" -lt 0 ] || [ "$port" -gt 65535 ]; th
     echo "'$port' is not valid port number (0-65535)"
     exit 1
 fi
+
+if [ "$EUID" -ne 0 ]; then
+    echo "Root privileges are needed to kill ports"
+    exit 1
+fi
+
+if [ -n "$(lsof -i :$port)" ]; then
+    echo "Port $port is in use"
+    echo "Killing process..."
+    lsof -i :$port | awk 'NR!=1 {print $2}' | xargs kill -9
+    echo "Done"
+else
+    echo "Port $port is not in use"
+fi
